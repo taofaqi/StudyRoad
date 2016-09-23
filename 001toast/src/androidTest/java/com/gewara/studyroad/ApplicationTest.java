3fd43bb167cc6098e9993a9538ef6a9e255ac044
陶发齐 <faqi.tao@gewara.com>
2016-09-23 17:44:36 +0800

    初始化第一个示例

---
+++ 001toast/src/androidTest/java/com/gewara/studyroad/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.gewara.studyroad;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
